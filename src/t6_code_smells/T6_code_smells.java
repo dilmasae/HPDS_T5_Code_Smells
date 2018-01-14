3504a1536781b6a1e9c76beeb06f3569e8dd4be4
Dilma Sae <dilmasae@gmail.com>
2018-01-14 21:06:20 +0000

    long list of parameters (problem)

--- src/t6_code_smells/T6_code_smells.java
+++ src/t6_code_smells/T6_code_smells.java
@@ -21,9 +21,13 @@ public class T6_code_smells {
         return Math.random() < 0.5;
     }
     
-    // DEAD CODE AFTER DELETE wasInitialized method
-    static double basePrice(double quantity, double itemPrice) {
-        return quantity * itemPrice;
+    static String[] long_list_of_parameters(
+            String name1, String name2, String name3, String name4, String name5,
+            String name6, String name7, String name8, String name9) {
+        
+        String[] array = new String[] {
+            name1, name2, name3, name4, name5, name6, name7, name8, name9};
+        return array;
     }
     
     public static void main(String[] args) {
